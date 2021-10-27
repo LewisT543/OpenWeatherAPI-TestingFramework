5bce0084db8bfe1bbc765497df71c7e6a5e53252
Natasha <natasha.pearl08@gmail.com>
2021-10-27 16:54:28 +0100

    updated+refactored project structure

--- src/main/java/Main_Alt.java
+++ src/main/java/Main_Alt.java
@@ -1,12 +1,12 @@
-import com.sparta.old.connection.ConnectionManager;
-import com.sparta.old.dto.ResponseDTO;
-import com.sparta.old.injector.Injector;
+import com.sparta.connection.ConnectionManager_Alt;
+import com.sparta.dto.ResponseDTO;
+import com.sparta.injector.Injector_Alt;
 
 // made by natasha
 
 public class Main_Alt {
     public static void main(String[] args) {
-        ResponseDTO responseDTO = Injector.injectResponseDTO(ConnectionManager.getConnection("q", "London"));
+        ResponseDTO responseDTO = Injector_Alt.injectResponseDTO(ConnectionManager_Alt.getConnection("q", "London"));
         System.out.println(responseDTO);
     }
 }
