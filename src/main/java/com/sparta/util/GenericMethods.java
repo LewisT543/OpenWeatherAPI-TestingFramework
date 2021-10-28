51d2809534f86379592fcefb1ec29e313a577d4a
Gareth <GMitchell@Spartaglobal.com>
2021-10-28 10:54:22 +0100

    Added Load City list method

--- src/main/java/com/sparta/util/GenericMethods.java
+++ src/main/java/com/sparta/util/GenericMethods.java
@@ -1,2 +1,20 @@
-package com.sparta.util;public class GenericMethods {
+package com.sparta.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class GenericMethods {
+
+    public static JsonNode loadCityList() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = objectMapper.readTree(GenericMethods.class.getResource("city.list.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonNode;
+    }
 }
