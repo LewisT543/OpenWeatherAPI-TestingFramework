c72a1a418c40eefc4fbe242a0776ce42cfb0b1b6
Dwhite77 <dan_white18@live.com>
2021-10-28 14:23:11 +0100

    fixed conflicts when merging pedros work in

--- src/test/java/com/sparta/util/genericmethods/GenericMethodTests.java
+++ src/test/java/com/sparta/util/genericmethods/GenericMethodTests.java
@@ -215,55 +215,9 @@ public class GenericMethodTests {
         }
     }
 
-
     @Nested
-    @DisplayName("isInteger Tests")
-    class isIntegerTests{
-        private int anInt = 5;
-        private Integer aInteger = 7;
-        private String aString = "A Test";
-        private Double aDouble = 7.4312;
-
-
-        @Test
-        @DisplayName("Given an integer type object return true")
-        void givenIntegerClassReturnTrue() {
-            Assertions.assertTrue(GenericMethods.isInteger(aInteger));
-        }
-
-        @Test
-        @DisplayName("Given an integer primitive type object return true")
-        void givenIntegerPrimitiveReturnTrue(){
-            Assertions.assertTrue(GenericMethods.isInteger(anInt));
-        }
-
-        @Test
-        @DisplayName("Given null return false")
-        void givenNullReturnFalse() {
-            Assertions.assertFalse(GenericMethods.isInteger(null));
-        }
-
-        @Test
-        @DisplayName("Given a string object return false")
-        void givenStringObjectReturnFalse() {
-            Assertions.assertFalse(GenericMethods.isInteger(aString));
-        }
-
-        @Test
-        @DisplayName("Given a double object return false")
-        void givenDoubleObjectReturnFalse() {
-            Assertions.assertFalse(GenericMethods.isInteger(aDouble));
-        }
-
-    }
-
-    @Nested
-    @DisplayName("isString Tests")
-    class isStringTests{
-        private char aChar = 'a';
-        private Integer aInteger = 7;
-        private String aString = "A Test";
-        private Double aDouble = 7.4312;
+    @DisplayName("CityIDTests")
+    class cityIdTests {
 
 
         @Test
@@ -287,17 +241,8 @@ public class GenericMethodTests {
             );
         }
 
-        @Test
-        @DisplayName("Given a char return false")
-        void givenCharReturnFalse(){
-            Assertions.assertFalse(GenericMethods.isString(aChar));
-        }
 
         @Test
-        @DisplayName("Given null return false")
-        void givenNullReturnFalse(){
-            Assertions.assertFalse(GenericMethods.isString(null));
-
         @DisplayName("CityID Returns false if Lat is correct")
         void CityIDReturnsFalseIfLonIsIncorrect() {
             assertFalse(GenericMethods.cityIdIsCorrect("Gerton", 0));
@@ -363,13 +308,14 @@ public class GenericMethodTests {
         void cityLatReturnsFalseIfCityIsIncorrect() {
             assertFalse(GenericMethods.cityLatIsCorrect("Incorrect", 35.479167));
         }
-
+    }
         @Nested
         @DisplayName("isString Tests")
-        class isStringTests {
-            Integer aInteger = 7;
-            String aString = "A Test";
-            Double aDouble = 7.4312;
+        class isStringTests{
+            private char aChar = 'a';
+            private Integer aInteger = 7;
+            private String aString = "A Test";
+            private Double aDouble = 7.4312;
 
             @Test
             @DisplayName("Given an string type object return true")
@@ -384,6 +330,13 @@ public class GenericMethodTests {
             }
 
             @Test
+            @DisplayName("Given a char return false")
+            void givenCharReturnFalse(){
+                Assertions.assertFalse(GenericMethods.isString(aChar));
+            }
+
+
+            @Test
             @DisplayName("Given an integer object return false")
             void givenIntegerObjectReturnFalse() {
                 Assertions.assertFalse(GenericMethods.isString(aInteger));
@@ -396,6 +349,47 @@ public class GenericMethodTests {
             }
         }
 
+    @Nested
+    @DisplayName("isInteger Tests")
+    class isIntegerTests{
+        private int anInt = 5;
+        private Integer aInteger = 7;
+        private String aString = "A Test";
+        private Double aDouble = 7.4312;
+
+
+        @Test
+        @DisplayName("Given an integer type object return true")
+        void givenIntegerClassReturnTrue() {
+            Assertions.assertTrue(GenericMethods.isInteger(aInteger));
+        }
+
+        @Test
+        @DisplayName("Given an integer primitive type object return true")
+        void givenIntegerPrimitiveReturnTrue(){
+            Assertions.assertTrue(GenericMethods.isInteger(anInt));
+        }
+
+        @Test
+        @DisplayName("Given null return false")
+        void givenNullReturnFalse() {
+            Assertions.assertFalse(GenericMethods.isInteger(null));
+        }
+
+        @Test
+        @DisplayName("Given a string object return false")
+        void givenStringObjectReturnFalse() {
+            Assertions.assertFalse(GenericMethods.isInteger(aString));
+        }
+
+        @Test
+        @DisplayName("Given a double object return false")
+        void givenDoubleObjectReturnFalse() {
+            Assertions.assertFalse(GenericMethods.isInteger(aDouble));
+        }
+
+    }
+
         @AfterEach
         void tearDown(TestInfo testInfo) {
             System.out.println(testInfo.getDisplayName() + " : END");
@@ -407,5 +401,5 @@ public class GenericMethodTests {
 
         }
 
-    }
+
 }
