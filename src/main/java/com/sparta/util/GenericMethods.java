3d064ae9dd7e93252c0f771ffe91d4e90891bdf9
Dwhite77 <dan_white18@live.com>
2021-10-28 14:35:17 +0100

    reremoved depricated methods

--- src/main/java/com/sparta/util/GenericMethods.java
+++ src/main/java/com/sparta/util/GenericMethods.java
@@ -91,17 +91,6 @@ public class GenericMethods {
         return isGreaterThanOrEqualToZero(input);
     }
 
-    /**
-     * @deprecated dan rewrote his method to implement this method's functionality
-     */
-    @Deprecated
-    public static boolean isGreaterThan(Double num, Double field) {
-        if (num == null || field == null) {
-            throw new NullPointerException("bro why'd you give me a null value. why'd you do this to me man. " +
-                    "i trusted you bro. how could you");
-        }
-        return field > num;
-    }
 
     public static boolean isADouble(Object field) {
         if(field instanceof Double) {
