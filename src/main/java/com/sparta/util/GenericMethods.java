25e6f4e8e1adbc93f1b137e145facea527732d31
Natasha <natasha.pearl08@gmail.com>
2021-10-28 12:24:19 +0100

    added unit tests for deprecated isGreaterThan() method

--- src/main/java/com/sparta/util/GenericMethods.java
+++ src/main/java/com/sparta/util/GenericMethods.java
@@ -10,7 +10,12 @@ public class GenericMethods {
         return input == null|| input >= 0;
     }
 
+    @Deprecated
     public static boolean isGreaterThan(Double num, Double field) {
+        if (num == null || field == null) {
+            throw new NullPointerException("bro why'd you give me a null value. why'd you do this to me man. " +
+                    "i trusted you bro. how could you");
+        }
         return field > num;
     }
 
--- src/test/java/com/sparta/util/genericmethods/GenericMethodTests.java
+++ src/test/java/com/sparta/util/genericmethods/GenericMethodTests.java
@@ -1,12 +1,21 @@
 package com.sparta.util.genericmethods;
 
 import com.sparta.util.GenericMethods;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class GenericMethodTests {
+
+    @BeforeAll
+    static void initAll(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName() + " starting\n-----------");
+    }
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName() + " : START");
+    }
 
     @Nested
     @DisplayName("IsGreaterThanZeroTests")
@@ -37,5 +46,68 @@ public class GenericMethodTests {
 
     }
 
+    @Nested
+    @DisplayName("Checking if Double is greater than number")
+    class CheckIsGreaterThan {
+        @Test
+        @DisplayName("Check one is greater than zero")
+        void checkOneIsGreaterThanZero() {
+            Assertions.assertTrue(GenericMethods.isGreaterThan(Double.valueOf(0), Double.valueOf(1)));
+        }
+
+        @Test
+        @DisplayName("Check zero is not greater than one")
+        void checkZeroIsNotGreaterThanOne() {
+            Assertions.assertFalse(GenericMethods.isGreaterThan(Double.valueOf(1), Double.valueOf(0)));
+        }
+
+        @Test
+        @DisplayName("Check that positive number is greater than negative number")
+        void checkThatPositiveNumberIsGreaterThanNegativeNumber() {
+            Assertions.assertTrue(GenericMethods.isGreaterThan(Double.valueOf(-1), Double.valueOf(1)));
+        }
+
+        @Test
+        @DisplayName("Check that negative number is not greater than positive number")
+        void checkThatNegativeNumberIsNotGreaterThanPositiveNumber() {
+            Assertions.assertFalse(GenericMethods.isGreaterThan(Double.valueOf(1), Double.valueOf(-1)));
+        }
+
+        @Test
+        @DisplayName("Check that 1.56 is greater than 1.33")
+        void checkThat1Point56IsGreaterThan1Point33() {
+            Assertions.assertTrue(GenericMethods.isGreaterThan(Double.valueOf(1.33), Double.valueOf(1.56)));
+        }
+
+        @Test
+        @DisplayName("Check that 1.33 is not greater than 1.56")
+        void checkThat1Point33IsNotGreaterThan1Point56() {
+            Assertions.assertFalse(GenericMethods.isGreaterThan(Double.valueOf(1.56), Double.valueOf(1.33)));
+        }
+
+        @Test
+        @DisplayName("Check that NullPointerException is thrown when either parameter is null")
+        void checkThatNullPointerExceptionIsThrownWhenEitherParameterIsNull() {
+            Assertions.assertThrows(NullPointerException.class, () -> {
+                GenericMethods.isGreaterThan(null, Double.valueOf(1));
+            });
+            Assertions.assertThrows(NullPointerException.class, () -> {
+                GenericMethods.isGreaterThan(Double.valueOf(1), null);
+            });
+            Assertions.assertThrows(NullPointerException.class, () -> {
+                GenericMethods.isGreaterThan(null, null);
+            });
+        }
+    }
+
+    @AfterEach
+    void tearDown (TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName() + " : END");
+    }
+
+    @AfterAll
+    static void tearDownAll(TestInfo testInfo) {
+        System.out.println("-----------\n" + testInfo.getDisplayName() + " completed");
+    }
 
 }
