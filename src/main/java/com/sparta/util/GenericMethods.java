5fcec52cd6e3c9525f324eeb7d047dbb4b52af35
Dwhite77 <dan_white18@live.com>
2021-10-28 10:51:18 +0100

    same as previous

--- src/main/java/com/sparta/util/GenericMethods.java
+++ src/main/java/com/sparta/util/GenericMethods.java
@@ -1,2 +1,14 @@
-package com.sparta.util;public class GenericMethods {
+package com.sparta.util;
+
+public class GenericMethods {
+
+    public static boolean isGreaterThanZero(Double input){
+        return input >= 0;
+    }
+
+    public static boolean isGreaterThanZeroOrNull(Double input){
+            return input == null|| input >= 0;
+    }
+
+
 }
--- src/test/java/com/sparta/util/genericmethods/GenericMethodTests.java
+++ src/test/java/com/sparta/util/genericmethods/GenericMethodTests.java
@@ -1,2 +1,41 @@
-package com.sparta.util.genericmethods;public class IsGreaterThanZero {
+package com.sparta.util.genericmethods;
+
+import com.sparta.util.GenericMethods;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class GenericMethodTests {
+
+    @Nested
+    @DisplayName("IsGreaterThanZeroTests")
+    class isGreaterThanZeroTests {
+        @Test
+        @DisplayName("Given 2 return true")
+        void givenAValueAboveZeroReturnTrue() {
+            Assertions.assertTrue(GenericMethods.isGreaterThanZero(2.0));
+        }
+
+        @Test
+        @DisplayName("Given -1 return false")
+        void given1ReturnFalse() {
+            Assertions.assertFalse(GenericMethods.isGreaterThanZero(-1.0));
+        }
+
+        @Test
+        @DisplayName("Given 0 return true")
+        void given0ReturnTrue() {
+            Assertions.assertTrue(GenericMethods.isGreaterThanZero(0.0));
+        }
+
+//        @Test
+//        @DisplayName("Given Null return false")
+//        void givenNullReturnFalse() {
+//            Assertions.assertFalse(GenericMethods.isGreaterThanZero(null));
+//        }
+
+    }
+
+
 }
