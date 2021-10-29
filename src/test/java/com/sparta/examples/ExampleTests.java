ec6e0e893e6b53386a2d6f0c8c6957f3e4df7194
Natasha <natasha.pearl08@gmail.com>
2021-10-29 15:55:50 +0100

    started writing longitude and latitude validity tests. hi manish

--- src/test/java/com/sparta/examples/ExampleTests.java
+++ src/test/java/com/sparta/examples/ExampleTests.java
@@ -1,12 +1,23 @@
 package com.sparta.examples;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.connection.ConnectionManager;
 import com.sparta.dto.ResponseDTO;
 import com.sparta.injector.Injector;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +26,8 @@ public class ExampleTests {
     private static ConnectionManager connectionManager;
     private static HashMap<String, String> params;
     private static ResponseDTO rDTO;
+    private static final String ROOT = "https://api.openweathermap.org/data/2.5/weather";
+    static HttpClient client = HttpClient.newHttpClient();
 
     @BeforeAll
     static void initAll(TestInfo testInfo) {
@@ -48,10 +61,30 @@ public class ExampleTests {
     @Nested
     @DisplayName("Not Using the framework")
     class notUsingTheFramework {
+
         @Test
-        @DisplayName("")
-        void methodName() {
-            
+        @DisplayName("Check that longitude and latitude are valid")
+        void checkThatLongitudeAndLatitudeAreValid() {
+            // yay manual connection manager
+            HttpRequest req = HttpRequest
+                    .newBuilder()
+                    .uri(URI.create("http://shibe.online/api/shibes")) // uniform resource indicator
+                    .build();
+            HttpResponse<String> resp = null;
+            try {
+                resp = client.send(req, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            JSONParser jsonParser = new JSONParser();
+            try {
+                JSONObject obj = (JSONObject) jsonParser.parse(resp.body());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
     }
     
